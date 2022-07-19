b026a34e1229ea30f27f5ec723ca230429f237c5
15227241216 <1469442737@qq.com>
2022-07-19 21:51:10 +0800

    chore: 更新配置项 初始化布局 :nerd_face:

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+  import { Repl } from '@vue/repl'
+  import '@vue/repl/style.css'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Repl />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
